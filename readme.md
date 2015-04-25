fe76a73ffc6500fb7d971abb03159a03fd6b08a5
Dave Smith-Hayes <dsmithhayes@gmail.com>
2015-04-25 18:16:51 -0400

    Cool little script.

---
+++ readme.md
@@ -0,0 +1,4 @@
+# weather.php
+
+This is a small script that tells me, personally, what the weather
+is in Toronto at the given time.
---
+++ weather.php
@@ -0,0 +1,30 @@
+#!/usr/bin/env php
+<?php namespace DSH\weather;
+
+$city = "Toronto";      // capitalized city name
+$country = "ca";        // lowercase country code
+
+$url  = "http://api.openweathermap.com/data/2.5/weather?q=";
+$url .= $city . "," . $country;
+
+$ch = curl_init()
+    or die("curl_init" . curl_error($ch));
+
+curl_setopt_array($ch, array(
+    CURLOPT_URL => $url,
+    CURLOPT_HEADER => false,
+    CURLOPT_RETURNTRANSFER => true
+));
+
+$json = curl_exec($ch)
+    or die("curl_exec" . curl_error($ch));
+
+$json = json_decode($json);
+
+curl_close($ch);
+
+echo "Hey Dave, the " . $json->weather[0]->description . " and ";
+echo "it is " . ($json->main->temp - 272.15) . " (C) degrees in ";
+echo $json->name . " right now.\n";
+
+?>

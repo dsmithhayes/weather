c72ba0a471929e82a6e3e8153b839b63b6cfffa2
Dave Smith-Hayes <dsmithhayes@gmail.com>
2015-05-23 00:45:04 -0400

    Sensible commenting.

--- weather.php
+++ weather.php
@@ -1,14 +1,14 @@
 #!/usr/bin/env php
 <?php namespace DSH\weather;
 
-$city = "Toronto";      // capitalized city name
-$country = "ca";        // lowercase country code
+$city = "Toronto";  // ?=Toronto,ca
+$country = "ca";
 
 $url  = "http://api.openweathermap.com/data/2.5/weather?q=";
 $url .= $city . "," . $country;
 
 $ch = curl_init()
-    or die("curl_init" . curl_error($ch));
+    or die("curl_init: " . curl_error($ch));
 
 curl_setopt_array($ch, array(
     CURLOPT_URL => $url,
@@ -17,14 +17,14 @@ curl_setopt_array($ch, array(
 ));
 
 $json = curl_exec($ch)
-    or die("curl_exec" . curl_error($ch));
+    or die("curl_exec: " . curl_error($ch));
 
 $json = json_decode($json);
 
 curl_close($ch);
 
 echo $json->weather[0]->description . "\n";
-echo ($json->main->temp - 272.15) . " (C) degrees in ";
+echo ($json->main->temp - 272.15) . " (C) degrees in "; // kelvin to celsius
 echo $json->name . " right now.\n";
 
 ?>
